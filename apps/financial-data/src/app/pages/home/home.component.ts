9b6d369edc3e331f193d68b128c316a8b8f0fa61
Emanuel Guével <emanuel.guevel@sib.fr>
2022-12-07 17:14:18 +0100

    Ajustement de la configuration des colonnes

--- apps/financial-data/src/app/pages/home/home.component.ts
+++ apps/financial-data/src/app/pages/home/home.component.ts
@@ -18,34 +18,27 @@ export class HomeComponent {
   groupingColumns: GroupingColumn[] = [
     {columnName: 'Theme'},
     {columnName: 'nom_programme'},
-    {columnName: 'label ref programmation'},
+    {columnName: '*crte'},
   ];
 
   searchInProgress = false;
 
   constructor() {
-    const moneyFormat = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' });
+    const moneyFormat = new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'});
     const dateFormat = (dateStr: string) => dateStr ? this.datePipe.transform(dateStr, 'shortDate') : '';
 
     this.columnsMetaData = new ColumnsMetaData([
       {name: 'Theme', label: 'Thème'},
       {name: 'nom_programme', label: 'Programme'},
-      {name: 'label ref programmation', label: 'Réf. programmation'},
-      //{name: 'code_programme', label: 'code_programme'},
-      {name: 'Commune', label: 'Commune', renderFn: (row, col) => row[col.name]?.['LabelCrte'] },
-      //{name: 'code_commune', label: 'code_commune'},
+      {name: '*crte', label: 'CRTE', renderFn: (row, _col) => row['Commune']?.['LabelCrte']},
       {name: 'nom_beneficiaire', label: 'Bénéficiaire'},
       {name: 'code_siret', label: 'Siret'},
       {name: 'type_etablissement', label: `Type d'établissement`},
-      {name: 'NEj', label: 'NEj'},
-      {name: 'NPosteEj', label: 'NPosteEj'},
       {
         name: 'DateModificationEj',
-        label: 'DateModificationEj',
+        label: 'Date modification EJ',
         renderFn: (row, col) => row[col.name] ? dateFormat(row[col.name]) : row[col.name]
       },
-      {name: 'CompteBudgetaire', label: 'Compte budgetaire'},
-      {name: 'ContratEtatRegion', label: 'Contrat État-Région'},
       {
         name: 'Montant', label: 'Montant',
         renderFn: (row, col) => row[col.name] ? moneyFormat.format(row[col.name]) : row[col.name],
