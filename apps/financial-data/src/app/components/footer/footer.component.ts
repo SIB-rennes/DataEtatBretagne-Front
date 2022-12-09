823866316ea86e0460cd99e941e5634609819296
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2022-12-09 16:22:28 +0100

    [FEAT] amélioration style footer

--- apps/financial-data/src/app/components/footer/footer.component.ts
+++ apps/financial-data/src/app/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'financial-footer',
   templateUrl: './footer.component.html',
-  styleUrls: [],
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   constructor() {}
