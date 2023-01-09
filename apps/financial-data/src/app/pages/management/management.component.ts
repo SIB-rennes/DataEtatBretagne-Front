2318bb72e302fb185c32cbb236a5197131fe9c71
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2023-01-09 17:49:55 +0100

    [FEAT] Ajout des services de changement de page et activation/désactiviation d'un user

--- apps/financial-data/src/app/pages/management/management.component.ts
+++ apps/financial-data/src/app/pages/management/management.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { UserHttpService } from '../../services/management/users-http.service';
+import { UserHttpService } from '@services/management/users-http.service';
 import { ActivatedRoute } from '@angular/router';
 import { User, UsersPagination } from '../../models/users/user.models';
+import { getFrenchPaginatorIntl } from '../../shared/paginator/french-paginator-intl';
+import { LoaderService } from '@services/loader.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'financial-management',
   styleUrls: [],
   standalone: true,
   templateUrl: './management.component.html',
+  providers: [
+    { provide: MatPaginatorIntl, useValue: getFrenchPaginatorIntl() },
+  ],
   imports: [
     MatTableModule,
     MatPaginatorModule,
@@ -27,21 +38,40 @@ export class ManagementComponent implements OnInit {
     'enabled',
   ];
 
+  @ViewChild(MatPaginator, { static: true }) paginator:
+    | MatPaginator
+    | undefined;
   public dataSource: UsersPagination = { users: [] };
 
   constructor(
     private userService: UserHttpService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private loading: LoaderService
   ) {}
 
   ngOnInit(): void {
     this.route.data.subscribe(
       (response: { usersPagination: UsersPagination | Error } | any) => {
         this.dataSource = response.usersPagination;
-        console.log(this.dataSource);
       }
     );
   }
 
   toggleActif(user: User): void {}
+
+  changePage(event: PageEvent): void {
+    // TODO fait un httpInterceptor pour le loader
+    this.loading.startLoader();
+
+    this.userService
+      .getUsers(event.pageIndex + 1, event.pageSize)
+      .pipe(
+        finalize(() => {
+          this.loading.endLoader();
+        })
+      )
+      .subscribe((userPagination: UsersPagination) => {
+        this.dataSource = userPagination;
+      });
+  }
 }
--- apps/financial-data/src/app/services/management/users-http.service.ts
+++ apps/financial-data/src/app/services/management/users-http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { SettingsService } from '../../../environments/settings.service';
 import { UsersPagination } from '@models/users/user.models';
 import { Observable } from 'rxjs';
@@ -12,13 +12,31 @@ export class UserHttpService {
   constructor(private http: HttpClient, private settings: SettingsService) {}
 
   public getUsers(
-    pageNumber: number = 0,
-    limit: number = 10
+    pageNumber: number = 1,
+    limit: number = 2
   ): Observable<UsersPagination> {
     const apiManagement = this.settings.apiManagement;
 
     const params = `pageNumber=${pageNumber}&limit=${limit}`;
 
     return this.http.get<UsersPagination>(`${apiManagement}/users/?${params}`);
+  }
+
+  public disableUser(uuid: string): Observable<string> {
+    const apiManagement = this.settings.apiManagement;
+
+    return this.http.patch<string>(
+      `${apiManagement}/users/disable/${uuid}`,
+      null
+    );
+  }
+
+  public enableUser(uuid: string): Observable<string> {
+    const apiManagement = this.settings.apiManagement;
+
+    return this.http.patch<string>(
+      `${apiManagement}/users/enable/${uuid}`,
+      null
+    );
   }
 }
