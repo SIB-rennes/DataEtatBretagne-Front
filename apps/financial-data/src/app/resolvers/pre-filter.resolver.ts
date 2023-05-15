7deb7ff10f8ed2d1e3b4e1a0ab82bdd4f8d1405e
gautierr <romain.gautier@sib.fr>
2023-05-15 14:48:33 +0200

    [REFACTO] Nettoyage pre-filter resolver

--- apps/financial-data/src/app/resolvers/pre-filter.resolver.ts
+++ apps/financial-data/src/app/resolvers/pre-filter.resolver.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { PreFilter } from '@models/search/prefilter.model';
 import { TOrError } from '@models/t-or-error.model';
 import { NGXLogger } from 'ngx-logger';
@@ -7,58 +7,36 @@ import { NGXLogger } from 'ngx-logger';
 export type PrefilterResolverModel = TOrError<PreFilter> | null
 
 export const resolvePreFilter: ResolveFn<PrefilterResolverModel> =
-    (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  (route: ActivatedRouteSnapshot) => {
 
-      let logger = inject(NGXLogger);
+    let logger = inject(NGXLogger);
 
-      let preFilters: PreFilter = {};
+    let preFilters: PreFilter = {};
 
-      let uuid = route.queryParamMap.get('uuid');
-      let programmes = route.queryParamMap.get('programmes');
-      let annee_min = route.queryParamMap.get('annee_min');
-      let annee_max = route.queryParamMap.get('annee_max')
+    let uuid = route.queryParamMap.get('uuid');
+    let programmes = route.queryParamMap.get('programmes');
 
-      if (uuid) {
-        logger.debug("Paramètre UUID présent. on ne calcule pas les filtres marque blanche");
-        return null;
+    if (uuid) {
+      logger.debug("Paramètre UUID présent. on ne calcule pas les filtres marque blanche");
+      return null;
+    }
+
+
+    if (programmes) {
+      let codes: string[] = programmes.split(',')
+      let bops = codes.map(code => {
+        return { 'Code': code }
+      });
+
+      preFilters = {
+        ...preFilters,
+        bops,
       }
+    }
 
-
-      if (programmes) {
-        let codes: string[] = programmes.split(',')
-        let bops = codes.map(code => {
-          return { 'Code': code }
-        });
-
-        preFilters = {
-          ...preFilters,
-          bops,
-        }
-      }
-
-      /*
-       *
-       * Année min
-       * Année max
-       * 
-       * 
-       */
-
-      // if (annee_min && _is_number(annee_min)) {
-      //   preFilters = {
-      //     ...preFilters,
-      //     // annee_min,
-      //   }
-      // }
-
-
-      if (Object.keys(preFilters).length === 0)
-        return null
-      return { 
-        data: preFilters
-      }
-    };
-
-function _is_number(x: any) {
-  return typeof x === 'number';
-}
+    if (Object.keys(preFilters).length === 0)
+      return null
+    return {
+      data: preFilters
+    }
+  };
