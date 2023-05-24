ce85e6d0d04cfa0f7be03426d6e6f4be0ee23dbb
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2023-05-24 16:10:50 +0200

    [FEAT] Ajout bouton pour supprimer utilisateur

--- apps/management/src/lib/components/management-user/management-user.component.ts
+++ apps/management/src/lib/components/management-user/management-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject, inject } from '@angular/core';
 import { MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { ActivatedRoute } from '@angular/router';
@@ -8,10 +8,10 @@ import { AlertService, SessionService } from 'apps/common-lib/src/public-api';
 import { getFrenchPaginatorIntl } from '../../shared/paginator/french-paginator-intl';
 import { User, UsersPagination } from '../../models/users/user.models';
 import { UserHttpService } from '../../services/users-http.service';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'lib-management-user',
-  styleUrls: [],
   templateUrl: './management-user.component.html',
   providers: [
     { provide: MatPaginatorIntl, useValue: getFrenchPaginatorIntl() },
@@ -24,6 +24,8 @@ export class ManagementUserComponent implements OnInit {
     'username',
     'enabled',
   ];
+
+  private dialog = inject(MatDialog);
 
   @ViewChild(MatCheckbox, { static: true }) public checkbox:
     | MatCheckbox
@@ -78,6 +80,39 @@ export class ManagementUserComponent implements OnInit {
     });
   }
 
+  public confirmDelete(user: User): void {
+    if (user.id === undefined) return;
+
+    const requestDelete = this.userService.deleteUsers(user.id);
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px',
+      data: 'Êtes-vous sûr de vouloir supprimer l\'utilisateur?'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        requestDelete.subscribe({
+            next: (_response: string) => {
+              this._retrieve_users(this.checkbox?.checked ?? false, 1).subscribe(
+                (userPagination: UsersPagination) => {
+                  this.dataSource = userPagination;
+                }
+              );
+
+              this.alertService.openAlertSuccess(
+                `Utilisateur ${user.email} supprimé avec succès.`
+              );
+            },
+            error: (error: any) => {
+              console.error(error);
+            },
+
+        })
+      }
+    });
+  }
+
   public onlyDisableUser(event: MatCheckboxChange): void {
     this._retrieve_users(event.checked).subscribe(
       (userPagination: UsersPagination) => {
@@ -103,4 +138,24 @@ export class ManagementUserComponent implements OnInit {
   ): Observable<UsersPagination> {
     return this.userService.getUsers(only_disable, pageIndex, pageSize);
   }
+}
+
+
+@Component({
+  selector: 'lib-management-confirm',
+  template: `
+    <h2 mat-dialog-title>Confirmation</h2>
+
+    <div mat-dialog-content>
+    {{ message }}
+    </div>
+    <mat-dialog-actions align="end">
+      <button mat-button color="warn" type="button" [mat-dialog-close]="true" cdkFocusInitial>Oui</button>
+      <button mat-button mat-dialog-close type="button">Non</button>
+    </mat-dialog-actions>
+  `
+})
+export class ConfirmationDialogComponent {
+  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public message: string) {}
 }
--- apps/management/src/lib/management.module.ts
+++ apps/management/src/lib/management.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
-import { ManagementUserComponent } from './components/management-user/management-user.component';
+import { ManagementUserComponent, ConfirmationDialogComponent } from './components/management-user/management-user.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [ManagementUserComponent],
+  declarations: [ManagementUserComponent, ConfirmationDialogComponent],
   imports: [
     MatTableModule,
+    MatTooltipModule,
     MatPaginatorModule,
+    MatDialogModule,
     MatSlideToggleModule,
     MatCardModule,
     MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
     CommonModule,
   ],
   exports: [ManagementUserComponent],
